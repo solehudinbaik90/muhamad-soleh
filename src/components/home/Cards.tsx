a5936877b033e3dce7d3371ea35d529dd8990637
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-17 14:24:41 +0700

    Add width and height to Image components
    Updated Image components to include width and height attributes for better performance.

--- src/components/home/Cards.tsx
+++ src/components/home/Cards.tsx
@@ -7,9 +7,9 @@ export default function Cards() {
         <div className="border-text grid gap-[30px] rounded-lg border-2 border-dotted p-[20px]">
           <h3 className="text-center text-[24px]">Layanan</h3>
           <div className="flex justify-around p-[10px]">
-            <Image src="/assets/images/icon/icon-2.png" alt="Ikon Layanan 1" />
-            <Image src="/assets/images/icon/icon-3.png" alt="Ikon Layanan 2" />
-            <Image src="/assets/images/icon/icon-4.png" alt="Ikon Layanan 3" />
+            <Image src="/assets/images/icon/icon-2.png" alt="Ikon Layanan 1" width={48} height={48} />
+            <Image src="/assets/images/icon/icon-3.png" alt="Ikon Layanan 2" width={48} height={48} />
+            <Image src="/assets/images/icon/icon-4.png" alt="Ikon Layanan 3" width={48} height={48} />
           </div>
         </div>
       </div>
@@ -18,7 +18,7 @@ export default function Cards() {
         <div className="border-text grid gap-[10px] rounded-lg border-2 border-dotted p-[20px]">
           <h3 className="text-center text-[24px]">Proyek</h3>
           <div className="flex justify-around">
-            <Image src="/assets/images/show/show-1.png" alt="Tampilan Proyek" />
+            <Image src="/assets/images/show/show-1.png" alt="Tampilan Proyek" width={220} height={140} />
           </div>
         </div>
       </div>
@@ -27,7 +27,7 @@ export default function Cards() {
         <div className="border-text grid gap-[38px] rounded-lg border-2 border-dotted p-[20px]">
           <h3 className="text-center text-[24px]">Profil</h3>
           <div className="flex justify-around p-[10px]">
-            <Image src="/assets/images/logo.svg" alt="Logo MSoleh" />
+            <Image src="/assets/images/logo.svg" alt="Logo MSoleh" width={120} height={40} />
           </div>
         </div>
       </div>
