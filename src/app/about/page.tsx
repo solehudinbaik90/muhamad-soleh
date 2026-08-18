0340000cd32f40f79d3b95390b2306ce0e209839
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-18 07:08:29 +0700

    Update personal information display in about page
    Replaced 'Email' section with 'Usia' and updated age.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -74,8 +74,8 @@ export default function AboutPage() {
                   </div>
                   <div className="mt-[20px] grid grid-cols-12">
                     <div className="col-span-6" data-aos="fade-up" data-aos-delay="250">
-                      <p className="text-[18px] font-medium">Email</p>
-                      <p className="mt-2 text-[16px] text-text">solehudinz369</p>
+                      <p className="text-[18px] font-medium">Usia</p>
+                      <p className="mt-2 text-[16px] text-text">27 Tahun</p>
                     </div>
                     <div className="col-span-6" data-aos="fade-up" data-aos-delay="250">
                       <p className="text-[18px] font-medium">Lokasi</p>
